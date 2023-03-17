e4705c32d70130008ca573cf9c3a4a422b2928f0
Leonardo <leonardodesantana05@gmail.com>
2023-03-17 19:48:12 -0300

    mobile test

--- public/js/modules/outsideClick.js
+++ public/js/modules/outsideClick.js
@@ -9,18 +9,13 @@ export default function outsideClick(element, events, callback) {
 
     element.setAttribute(outside, "");
   }
-
   function handleOutside(event) {
-    const isLink = event.target.nodeName === 'A';
-    const delay = isLink && 'ontouchstart' in window ? 300 : 0; // delay de 300ms para dispositivos móveis ao clicar em links
-    setTimeout(() => {
-      if (!element.contains(event.target) && !isLink) {
-        element.removeAttribute(outside);
-        events.forEach((userEvent) => {
-          html.removeEventListener(userEvent, handleOutside);
-        });
-        callback();
-      }
-    }, delay);
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutside);
+      });
+      callback();
+    }
   }
 }
